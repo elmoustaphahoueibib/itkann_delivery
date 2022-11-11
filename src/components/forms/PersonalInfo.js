75c993c39e0a4fca9fb785304eb7a254de2607c3
Houeibib Elmoustapha <31520397+elmoustaphahoueibib@users.noreply.github.com>
2022-11-11 13:28:56 +0000

    Update PersonalInfo.js

--- src/components/forms/PersonalInfo.js
+++ src/components/forms/PersonalInfo.js
@@ -27,11 +27,11 @@ const PersonalInfo = () => {
         <div className="contain">
             <p>Entrer vos informations</p>
             <form className="form">
-                <input className="formInput" type="text" placeholder="Nom" onChange={e => updateContext.setName(e.target.value)} required/>
-                <input className="formInput" type="text" placeholder="Prénom" onChange={e => updateContext.setLastName(e.target.value)} required/>
-                <input className="formInput" type="email" placeholder="Email" onChange={e => updateContext.setEmail(e.target.value)} required/>
-                <input className="formInput" type="text" placeholder="Numéro" maxLength="8" onChange={e => updateContext.setPhone(e.target.value)}  required/>
-                <input className="formInput" type="text" placeholder="Nni" maxLength="10" onChange={e => updateContext.setNni(e.target.value)}  required/>
+                <input className="formInput" type="text" placeholder="Nom" onChange={e => updateContext.setName(e.target.value)} required={true}/>
+                <input className="formInput" type="text" placeholder="Prénom" onChange={e => updateContext.setLastName(e.target.value)} required={true}/>
+                <input className="formInput" type="email" placeholder="Email" onChange={e => updateContext.setEmail(e.target.value)} required={true}/>
+                <input className="formInput" type="text" placeholder="Numéro" maxLength="8" onChange={e => updateContext.setPhone(e.target.value)}  required={true}/>
+                <input className="formInput" type="text" placeholder="Nni" maxLength="10" onChange={e => updateContext.setNni(e.target.value)}  required={true}/>
                 <button type="button" className="formSubmit" onClick={next} >Next</button>
             </form>
         </div>
